3e26fa10b90135d55194cf633f7d8ce6aaf0e251
mariaBX <maria@bitxenio.com>
2015-06-11 18:22:27 +0200

    Creditos

--- routes/index.js
+++ routes/index.js
@@ -9,5 +9,7 @@ router.get('/', function(req, res) {
 
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
-
+router.get('/author', function(req, res) {
+  res.render('../views/author', { title: 'Author' });
+});
 module.exports = router;
