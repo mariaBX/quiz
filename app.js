d498ce43bf2fd18cb57f82828d428b32287c5755
mariaBX <maria@bitxenio.com>
2015-06-11 10:23:07 +0200

    express-partials

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+//importar express-partials
+var partials = require('express-partials');
 
 var routes = require('./routes/index');
 
@@ -13,6 +15,9 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+//usar el express-partials
+app.use(partials());
 
 // uncomment after placing your favicon in /public
 app.use(favicon(__dirname + '/public/favicon.ico'));
